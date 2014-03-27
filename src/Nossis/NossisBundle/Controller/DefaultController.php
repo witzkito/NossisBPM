d2e80e1c3b948c0410dcddc0e9a966108b23dff1
witzkito <witzkito@gmail.com>
2014-03-27 20:15:25 -0300

    Agregado Ingresar Stock y cambios en CSS

--- src/Nossis/NossisBundle/Controller/DefaultController.php
+++ src/Nossis/NossisBundle/Controller/DefaultController.php
@@ -6,8 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('NossisBundle:Default:index.html.twig', array('name' => $name));
+        $form = $this->createFormBuilder()
+                        ->add('codigo', 'text', array("label" => false))
+                        ->getForm();
+        
+        $em = $this->get('doctrine')->getManager();
+        $almacenes = $em->getRepository('NossisBundle:Almacen')->findAll();
+        $productos = $em->getRepository('NossisBundle:Producto')->findAll();
+        $request = $this->get('request');
+        if ($request->getMethod() == 'POST'){
+            
+        }
+        
+        return $this->render('NossisBundle:Default:index.html.twig', array(
+                'form'    => $form->createView(),
+                'almacenes' => $almacenes,
+                'productos' => $productos,
+        ));
     }
 }
--- src/Nossis/NossisBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Nossis/NossisBundle/Tests/Controller/DefaultControllerTest.php
@@ -2,16 +2,22 @@
 
 namespace Nossis\NossisBundle\Tests\Controller;
 
+use Nossis\NossisBundle\Entity\Almacen;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
+    
     public function testIndex()
     {
+        
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
-
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $crawler = $client->request('GET', '/index');
+        
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("Codigo")')->count()
+        );
     }
 }
