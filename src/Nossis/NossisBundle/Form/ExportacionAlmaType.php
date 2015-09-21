ac62adede1e9a1d1a5b825a8b48d46c30319fc19
witzkito <witzkito@gmail.com>
2015-09-21 09:58:29 -0300

    Se agrego exportacion Alma

---
+++ src/Nossis/NossisBundle/Form/ExportacionAlmaType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Nossis\NossisBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ExportacionAlmaType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('fechaInicio', 'genemu_jquerydate', array(
+                'widget' => 'single_text'
+            ))
+            ->add('fechaFin', 'genemu_jquerydate', array(
+                'widget' => 'single_text'
+            ))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Nossis\NossisBundle\Entity\ExportacionAlma'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'nossis_nossisbundle_exportacionalma';
+    }
+}
